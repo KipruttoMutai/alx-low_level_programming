b95ac23dd6b5543a8fcea9ec32aca4dfa2e744de
KipruttoMutai <mutaibrian04@gmail.com>
2023-07-19 13:12:13 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 /**
-* main - prints the fibonacci sequence
-* starting with 1 and 2 
-* return 0 sucess
+* main - Entry point
+* Description; Prints sum of even value Fibonacci starting with 1 and 2
+* Return: Always  0 (sucess)
  */
-int main (void)
+int main(void)
 {
 	long int a = 1;
 	long int b = 2;
 	long int sum = b;
 
-	while(a + b <= 4000000)
+	while (a + b <= 4000000)
 	{
 	b += a;
 	if (b % 2 == 0)
 	sum += b;
 	a = b - a;
-	}	
-	printf("%ld\n",sum);
-	return 0;
+	}
+	printf("%ld\n", sum);
+	return (0);
 }
