a9b87cdfd72d039abcc3cc4e9a15718f7bc15c01
KipruttoMutai <mutaibrian04@gmail.com>
2023-07-19 11:11:43 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+* main-prints the fibonacci sequence
+* starting with 1 and 2 
+* return 0 sucess
+ */
+int main (void)
+{
+    long int a = 1;
+    long int b = 2;
+    long int sum = b;
+    while(a + b <= 4000000)
+    {
+        b += a;
+        if (b % 2 == 0)
+            sum += b;
+        a = b - a;
+    }
+    printf("%ld\n",sum);
+    return 0;
+}
