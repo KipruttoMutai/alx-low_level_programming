d4e57a382af39e89412fe6792ce164ec5387978e
KipruttoMutai <mutaibrian04@gmail.com>
2023-08-07 12:53:03 -0700

    function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
 /**
  * str_concat - returns concatenated string
  * @s1: first string
@@ -9,20 +8,35 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	char *s3;
+	int i, ci;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	size_t len_s1 = strlen(s1);
-	size_t len_s2 = strlen(s2);
-	size_t total_length = len_s1 + len_s2 + 1;
-	char *s3 = (char *)malloc(total_length);
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	s3 = malloc(sizeof(char) * (i + ci + 1));
 
 	if (s3 == NULL)
-	{
 		return (NULL);
+	i = ci = 0;
+
+	while (s1[i] != '\0')
+	{
+		s3[i] = s1[i];
+		i++;
 	}
-	strcpy(s3, s1);
-	strcat(s3, s2);
+	while (s2[ci] != '\0')
+	{
+		s3[i] = s2[ci];
+		i++, ci++;
+	}
+	s3[i] = '\0';
 	return (s3);
 }
+
