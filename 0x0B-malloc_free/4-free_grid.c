d1c0a7b1ac2ef0173c6cfa36635f537051d42563
KipruttoMutai <mutaibrian04@gmail.com>
2023-08-08 11:29:33 -0700

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,11 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *void free_grid()- frees a 2D array previously created by alloc
+ *Returns nothing
+ *
+ */
+void free_grid(int **grid, int height)
+{
+
+}
