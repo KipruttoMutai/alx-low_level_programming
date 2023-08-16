52c0990df6b90919c6313eebf46fc407e71439ca
KipruttoMutai <mutaibrian04@gmail.com>
2023-08-16 08:33:07 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-
 /**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the command-line arguments.
- * Return: 0 on success, other values on error.
+ * main - Prints the result of simple operations.
+ *@argc: The number of arguments supplied to the program.
+ *@argv: An array of pointers to the arguments.
+ * Return: Always 0.
  */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	char *operator;
+	int result;
+
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
+
+	operation = get_op_func(operator);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((*operator == '/' || *operator == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = operation(num1, num2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
 
