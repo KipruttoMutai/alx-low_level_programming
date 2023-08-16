bb27a3927eb64b971948b3a22cb929b3ea65eb9d
KipruttoMutai <mutaibrian04@gmail.com>
2023-08-16 08:02:44 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ * Return: 0 on success, other values on error.
+ */
+
