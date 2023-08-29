c8a996505cf2b50187cae2cc75d2ddd348763722
KipruttoMutai <mutaibrian04@gmail.com>
2023-08-29 12:00:29 -0700

    Print List

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num_nodes = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
+		count++;
 		h = h->next;
-		num_nodes++;
 	}
 
-	return (num_nodes);
+	return (count);
 }
