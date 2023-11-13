0e7b5661ba371346b05819bfed8c156e7b3d0a09
KipruttoMutai <mutaibrian04@gmail.com>
2023-11-13 15:48:11 +0000

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P 
+wget -P .. https://raw.githubusercontent.com/KipruttoMutai/alx-low_level_programming/master/0x18-dynamic_libraries/liback.so
+export LD_PRELOAD="PWD/../liback.so"
