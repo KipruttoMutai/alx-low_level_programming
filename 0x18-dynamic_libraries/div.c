8489d0f2a26630f79d3201e0e5b913334c8dad54
KipruttoMutai <mutaibrian04@gmail.com>
2023-11-13 15:03:22 +0000

    Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * div - divides a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: division of a and b
+ **/
+int div(int a, int b)
+{
+	return (a / b);
+}	
---
+++ 0x18-dynamic_libraries/funcs.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+/*
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ **/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/*
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Remainder of a and b
+ **/
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/*
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of a and b
+ **/
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/*
+ * mod - modulo two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of a and b
+ **/
+int mod(int a, int b)
+{
+        return (a % b);
+}
+
